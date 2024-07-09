6b7bb2d8f2f540dbbc5b8052beda14ec1d159ad6
Vidar Eldøy <vidar@eldoy.com>
2024-07-09 15:31:49 +0200

    Add portabletext toHTML

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var imageUrlBuilder = require('@sanity/image-url')
+var portableText = require('@portabletext/to-html')
 
-module.exports = function(config = {}) {
-
+module.exports = function (config = {}) {
   function urlFor(source) {
     return imageUrlBuilder(config).image(source)
   }
 
-  return { urlFor }
+  function toHTML(blocks) {
+    return portableText.toHTML(blocks)
+  }
+
+  return { urlFor, toHTML }
 }
--- spec/tests/unsanity-test.js
+++ spec/tests/unsanity-test.js
@@ -1,6 +1,6 @@
 var unsanity = require('../../index.js')
 
-it('should generate an image url', async function({ t }) {
+it('should generate an image url', async function ({ t }) {
   var config = {
     baseUrl: 'https://cdn.sanity.io',
     projectId: '671pyuqa',
@@ -10,5 +10,15 @@ it('should generate an image url', async function({ t }) {
   var image = 'image-5df93b336165f44fc1199c28b2697af7f4e7aeda-960x480-jpg'
   var result = urlFor(image).auto('format').fit('max').width(720).toString()
 
-  t.equal(result, 'https://cdn.sanity.io/images/671pyuqa/production/5df93b336165f44fc1199c28b2697af7f4e7aeda-960x480.jpg?w=720&fit=max&auto=format')
+  t.equal(
+    result,
+    'https://cdn.sanity.io/images/671pyuqa/production/5df93b336165f44fc1199c28b2697af7f4e7aeda-960x480.jpg?w=720&fit=max&auto=format'
+  )
+})
+
+it('should convert portable text to HTML', async function ({ t, data }) {
+  var { toHTML } = unsanity()
+  var result = toHTML(data.portabletext.description)
+
+  t.ok(result.startsWith('<p>'))
 })
